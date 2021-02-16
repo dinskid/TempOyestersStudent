8f7bbfb1e09e3a2cca47b1dd4298ece768e1e79e
hari-44456 <naraharipapshetwar@gmail.com>
2021-02-16 22:55:45 +0700

    changed axios default config

--- gogo-react/src/helpers/axiosInstance.js
+++ gogo-react/src/helpers/axiosInstance.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+
 const Instance = axios.create({
+  baseURL: `${window.location.protocol}//${window.location.hostname}`,
   credentials: 'include',
+  withCredentials: true,
 });
 
 export default Instance;
