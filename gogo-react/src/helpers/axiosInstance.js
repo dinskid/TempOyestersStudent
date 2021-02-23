b10bc94e8e824f50abea08d7626b3d4c90997f24
Jitul Teron <jitulteron9@gmail.com>
2021-02-23 12:02:21 +0530

    baseURL changes

--- gogo-react/src/helpers/axiosInstance.js
+++ gogo-react/src/helpers/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const Instance = axios.create({
   // baseURL: `${window.location.protocol}//${window.location.hostname}`,
-  // baseURL: `${window.location.protocol}//${window.location.hostname}:5000`,
-  baseURL:'http://localhost:5000/',
+  baseURL: `${window.location.protocol}//${window.location.hostname}:5000`,
+  // baseURL:'http://localhost:5000/',
   // credentials: 'include',
   headers:{
     "content-type":"application/json"
