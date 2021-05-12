d20694d122a37f05ef53c6a37383f56583f6a97b
Dv <dv@Dvs-MacBook-Pro.local>
2021-05-12 15:09:10 +0530

    changes

--- gogo-react/src/Student/user/login.js
+++ gogo-react/src/Student/user/login.js
@@ -56,7 +56,7 @@ const Login = ({ history, loading, error, loginUserAction, currentUser }) => {
     if (error === undefined) {
       NotificationManager.error(
         error,
-        'username or password is incorrect',
+        'Username or Password is incorrect',
         3000,
         null,
         null,
