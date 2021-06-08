c7ced891785d89ee1c8e5a066b235c8e41e1805e
Vedant Khandokar <67046274+VedWork19@users.noreply.github.com>
2021-06-08 12:24:19 +0530

    Update axiosInstance.js

--- gogo-react/src/helpers/axiosInstance.js
+++ gogo-react/src/helpers/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const Instance = axios.create({
   // baseURL: `${window.location.protocol}//${window.location.hostname}`,
-  baseURL: `${window.location.protocol}//${window.location.hostname}`,
+  baseURL: `${window.location.protocol}//${window.location.hostname}:5000`,
   // baseURL:'http://localhost:5000/',
   // credentials: 'include',
   headers: {
